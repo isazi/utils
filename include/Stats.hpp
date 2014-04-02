bba95278527f9fb069f275e93069c2ca4d910459
Alessio Sclocco <alessio@sclocco.eu>
2014-04-01 21:08:53 -0700

    New license: Apache 2.0.

--- include/Stats.hpp
+++ include/Stats.hpp
@@ -1,20 +1,16 @@
+// Copyright 2014 Alessio Sclocco <a.sclocco@vu.nl>
 //
-// Copyright (C) 2014
-// Alessio Sclocco <alessio@sclocco.eu>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as
-// published by the Free Software Foundation, either version 3 of the
-// License, or (at your option) any later version.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include <cmath>
 
@@ -52,7 +48,7 @@ template< typename T > Stats< T >::~Stats() {}
 
 template< typename T > inline void Stats< T >::addElement(T element) {
 	double oldAverage = average;
-	
+
 	nrElements++;
 
 	if ( nrElements == 1 ) {
@@ -74,7 +70,7 @@ template< typename T > inline long long unsigned int Stats< T >::getNrElements()
 }
 
 template< typename T > inline double Stats< T >::getAverage() const {
-	return average;	
+	return average;
 }
 
 template< typename T > inline double Stats< T >::getVariance() const {
